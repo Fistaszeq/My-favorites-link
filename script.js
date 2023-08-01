7ba76a7de6b3b0515b3624ca5e7c9cf9b7c88305
Fistaszeq <87309628+Fistaszeq@users.noreply.github.com>
2023-08-01 18:01:11 +0200

    Add files via upload
    Update

--- script.js
+++ script.js
@@ -23,3 +23,56 @@ document.getElementById("toggleButton5").addEventListener("click", function() {
 document.getElementById("toggleButton6").addEventListener("click", function() {
     document.getElementById("slideDiv6").classList.toggle("active");
 });
+
+document.getElementById("buttonmenu1").addEventListener("click", function() {
+    document.getElementById("menu1").classList.toggle("active");
+});
+
+    // Dark mode and Light mode 
+
+let mode = False;
+
+// [mode] it will be definition of witch 
+// mode is on and witch mode is off 
+// it will be somethink like this 
+// if mode = False 
+//     LightBtn();
+// if mode = True
+//     DarkBtn();
+
+function LightBtn() {
+
+    // Main site changing to lighter
+    document.getElementById("site").style.backgroundColor = "white";
+    document.getElementById("site").style.color = "Black";
+    document.getElementById("slideDiv").style.backgroundColor = "White";
+    document.getElementById("menu1").style.backgroundColor = "White";
+    document.getElementById("slideDiv2").style.backgroundColor = "White";
+    document.getElementById("slideDiv3").style.backgroundColor = "White";
+    document.getElementById("slideDiv4").style.backgroundColor = "White";
+    document.getElementById("slideDiv5").style.backgroundColor = "White";
+    document.getElementById("slideDiv6").style.backgroundColor = "White";
+
+    //buttons
+    document.getElementById("toggleButton").style.backgroundColor = "White";   
+    document.getElementById("toggleButton").style.color = "Black"; 
+
+}
+
+function DarkBtn() {
+
+    // Main site changing to darker
+    document.getElementById("site").style.backgroundColor = "Black";
+    document.getElementById("site").style.color = "White";
+    document.getElementById("toggleButton").style.backgroundColor = "Black";   
+    document.getElementById("toggleButton").style.color = "White"; 
+
+    document.getElementById("slideDiv").style.backgroundColor = "Black";
+    document.getElementById("menu1").style.backgroundColor = "Black";
+    document.getElementById("slideDiv2").style.backgroundColor = "Black";
+    document.getElementById("slideDiv3").style.backgroundColor = "Black";
+    document.getElementById("slideDiv4").style.backgroundColor = "Black";
+    document.getElementById("slideDiv5").style.backgroundColor = "Black";
+    document.getElementById("slideDiv6").style.backgroundColor = "Black";
+
+}
